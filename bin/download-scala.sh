1980ae68c9dba83d9fe3161959c2b50a09e781f9
Sam Zhao <sam@cs.brown.edu>
2015-07-28 07:23:12 -0700

    scripts: add download scala.

---
+++ bin/download-scala.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ -d /vagrant ]; then
+  source /vagrant/scripts/common.sh
+else
+  source $(dirname $0)/../scripts/common.sh
+fi
+
+wget http://downloads.typesafe.com/scala/2.11.4/scala-2.11.4.tgz?_ga=1.21732155.1693576347.1436951837 -O $PROJECT/scala-2.11.4.tgz
+ls
