1b236b4a417e9c9c27d7dd79fab504d647296cc2
Sam Zhao <sam@cs.brown.edu>
2015-07-29 06:46:09 -0700

    script: fix build-spark and enable options in bench-all.

--- bin/build-spark.sh
+++ bin/build-spark.sh
@@ -18,13 +18,16 @@ function buildSpark {
 
   (cd $PROJECT/third_party/spark && git reset --hard HEAD && $MAVEN clean)
 
-  if [ $SPARK_VER_MAJOR = "2.10" ]; then
+  if [ $SCALA_VER_MAJOR = "2.10" ]; then
     ./dev/change-version-to-2.10.sh
     ./make-distribution.sh --with-tachyon --skip-java-test --name hadoop-$HADOOP_VER_MAJOR-scala-2.10 --tgz -Phadoop-$HADOOP_VER_MAJOR -Pyarn
-  else
+  elif [ $SCALA_VER_MAJOR = "2.11" ]; then
     ./dev/change-version-to-2.11.sh
     #./make-distribution.sh --with-tachyon --skip-java-test --name hadoop-$HADOOP_VER_MAJOR --tgz -Phadoop-$HADOOP_VER_MAJOR -Pyarn -Dscala-2.11 
     ./make-distribution.sh --with-tachyon --skip-java-test --name hadoop-$HADOOP_VER_MAJOR-scala-2.11 --tgz -Phadoop-$HADOOP_VER_MAJOR -Pyarn -Dscala-2.11
+  else
+    echo "[error] unsupported scala version: $SCALA_VER_MAJOR"
+    exit 1
   fi
 }
 
--- study/bench/bench-all.sh
+++ study/bench/bench-all.sh
@@ -4,27 +4,79 @@ set -e
 
 bench_root=$(dirname $0)/cfsgd
 
+func_mode="test"
 benchs=("cc" "java" "scala")
 
-func_mode="test"
-if [ "$1" = "result" ]; then
-  func_mode="result"
-fi
+function show_help {
+  echo "usage: bench-all.sh -m <test|result> -b <cc|java|scala>..."
+}
 
-echo "[info] start bench master"
-for b in "${benchs[@]}"; do
-  echo "[info] start $bench_root/$b"
-  bash $bench_root/$b/bench-${b}.sh $func_mode
-  echo "[info] end $bench_root/$b"
-done
+function parse_args {
+  while :; do
+    case $1 in
+      -h|-\?|--help)   # Call a "show_help" function to display a synopsis, then exit.
+        show_help
+        exit
+        ;;
+      -m)
+        func_mode=$2
+        shift
+        ;;
+      -b)
+        benchs=( "${@:2}" )
+        shift ${#benchs[@]}
+        ;;
+      --)              # End of all options.
+        shift
+        break
+        ;;
+      -?*)
+        printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+        ;;
+      *)               # Default case: If no more options then break out of the loop.
+        break
+    esac
 
-if [ $func_mode = "test" ]; then
-  echo "[info] print test outcome"
-  for b in "${benchs[@]}"; do
-    echo "[info] $b test"
-    tail -n 2 $bench_root/$b/test/*
+    shift
   done
-  echo "[info] finish test outcome"
-fi
 
-echo "[info] end bench master"
+  if [ -z $func_mode ] || [ -z $benchs ]; then
+    echo "[error] wrong arguments."
+    show_help
+    exit 1
+  fi
+}
+
+function main {
+  echo "[info] start bench master"
+  for b in "${benchs[@]}"; do
+    echo "[info] start $bench_root/$b"
+    bash $bench_root/$b/bench-${b}.sh $func_mode
+    echo "[info] end $bench_root/$b"
+  done
+
+  if [ $func_mode = "test" ]; then
+    echo "[info] print test outcome"
+    for b in "${benchs[@]}"; do
+      echo "[info] $b test"
+      tail -n 2 $bench_root/$b/test/*
+    done
+    echo "[info] finish test outcome"
+  else
+    echo "[info] collect test outcome"
+    all_results=$bench_root/all-results
+    my_result=$all_results/result.`date +%Y%m%d.%H%M`
+    mkdir -p $all_results
+    mkdir $my_result
+    for b in "${benchs[@]}"; do
+      echo "[info] $b test"
+      mkdir -p $my_result/$b
+      mv $bench_root/$b/result/* $my_result/$b
+    done
+  fi
+
+  echo "[info] end bench master"
+}
+
+parse_args $@
+main
