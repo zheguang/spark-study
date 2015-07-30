2ed625d41b9b61d382caa4868f0184196f4f6c26
Sam Zhao <sam@cs.brown.edu>
2015-07-30 12:09:25 -0700

    tip: add java opts for OOME

--- bin/check-jvm.sh
+++ bin/check-jvm.sh
@@ -3,3 +3,6 @@
 set -e
 
 java -XX:+PrintFlagsFinal -version | egrep -i 'HeapSize|PermSize|THreadStateSize'
+
+# java opt
+#-server -XX:+HeapDumpOnOutOfMemoryError  -XX:ErrorFile=/var/local/java/hs_err_pid.log -XX:HeapDumpPath=/var/local/java
