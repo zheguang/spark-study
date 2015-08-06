4dfb25ea6a45a3d7290b483e2a4d13bbeaf2d28a
Sam Zhao <sam@cs.brown.edu>
2015-08-06 11:25:12 -0700

    SparkSgd: remove comments and move code around.

--- study/src/main/scala/edu/brown/cs/sparkstudy/SparkSgdNaive.scala
+++ study/src/main/scala/edu/brown/cs/sparkstudy/SparkSgdNaive.scala
@@ -9,83 +9,6 @@ import scala.collection.mutable
 
 object SparkSgdNaive extends Logging {
 
-  type UserId = Int
-  type ItemId = Int
-  type UsersBlockId = Int
-  type ItemsBlockId = Int
-
-  case class Rating(user: UserId, item: ItemId, rating: Double) extends Serializable
-
-  case class RatingsBlock(users: Array[UserId], items: Array[ItemId], ratings: Array[Double]) extends Serializable
-
-  case class UsersBlock(users: Array[UserId], factors: Array[Array[Double]]) extends Serializable
-
-  case class ItemsBlock(items: Array[ItemId], factors: Array[Array[Double]]) extends Serializable
-
-  class RatingsBlockBuilder extends Serializable {
-    val users  = mutable.ArrayBuilder.make[UserId]
-    val items = mutable.ArrayBuilder.make[ItemId]
-    val ratings = mutable.ArrayBuilder.make[Double]
-
-    def add(r: Rating): RatingsBlockBuilder = {
-      users += r.user
-      items += r.item
-      ratings += r.rating
-      this
-    }
-    
-    def merge(b: RatingsBlock): RatingsBlockBuilder = {
-      users ++= b.users
-      items ++= b.items
-      ratings ++= b.ratings
-      this
-    }
-
-    def build(): RatingsBlock = {
-      RatingsBlock(users.result(), items.result(), ratings.result())
-    }
-  }
-
-  class UsersBlockBuilder(factorLength: Int) extends Serializable {
-    val users = mutable.ArrayBuilder.make[UserId]
-
-    def add(us: Array[UserId]): UsersBlockBuilder = {
-      users ++= us
-      this
-    }
-    
-    def merge(b: UsersBlock): UsersBlockBuilder = {
-      users ++= b.users
-      this
-    }
-    
-    def build(): UsersBlock = {
-      val uniqueUsers = users.result().toSet.toSeq.sorted.toArray
-      val factors = Array.fill(uniqueUsers.length, factorLength)(randGen.nextDouble())
-      UsersBlock(uniqueUsers, factors)
-    }
-  }
-  
-  class ItemsBlockBuilder(factorLength: Int) extends Serializable {
-    val items = mutable.ArrayBuilder.make[ItemId]
-
-    def add(us: Array[UserId]): ItemsBlockBuilder = {
-      items ++= us
-      this
-    }
-
-    def merge(b: ItemsBlock): ItemsBlockBuilder = {
-      items ++= b.items
-      this
-    }
-
-    def build(): ItemsBlock = {
-      val uniqueItems = items.result().toSet.toSeq.sorted.toArray
-      val factors = Array.fill(uniqueItems.length, factorLength)(randGen.nextDouble())
-      ItemsBlock(uniqueItems, factors)
-    }
-  }
-
   def main(args: Array[String]) = {
     if (args.length < argNames.length) {
       printUsage("SparkSgd")
@@ -95,9 +18,9 @@ object SparkSgdNaive extends Logging {
 
     val num_latent = args(0).toInt
     val filePath = args(1)
-    val num_users = args(2).toInt // fixme: never used
-    val num_items = args(3).toInt // fixme: never used
-    val num_ratings = args(4).toInt // fixme: never used
+    val num_users = args(2).toInt // never used, will compute from ratings
+    val num_items = args(3).toInt // never used, will compute from ratings
+    val num_ratings = args(4).toInt // never used, will compute from ratings
     val num_procs = args(5).toInt
     val num_nodes = 1
 
@@ -109,7 +32,7 @@ object SparkSgdNaive extends Logging {
 
     val numUserBlocks = num_nodes * num_procs
     val numItemBlocks = num_nodes * num_procs
-    println(s"[sam] init: numUserBlocks: $numUserBlocks, numItemBlocks: $numItemBlocks")
+    println(s"[sam] init: ratings count: ${ratings.count()}, numUserBlocks: $numUserBlocks, numItemBlocks: $numItemBlocks")
 
     val userPart = new HashPartitioner(numUserBlocks)
     val itemPart = new HashPartitioner(numItemBlocks)
@@ -119,7 +42,9 @@ object SparkSgdNaive extends Logging {
     // materialize
     usersBlocks.count()
     itemsBlocks.count()
+
     println(s"[sam] init: ratingsblocks count: ${ratingsBlocks.count()}, usersBlocks count: ${usersBlocks.count()}, itemsBlocks count: ${itemsBlocks.count()}")
+    println(s"[sam] init: numUsers: ${usersBlocks.mapValues(_.users.length).values.sum()}, numItems: ${itemsBlocks.mapValues(_.items.length).values.sum()}")
 
     val maxNumIters = 5
     val numDiagnalRounds = itemsBlocks.count().toInt
@@ -131,9 +56,7 @@ object SparkSgdNaive extends Logging {
         val ratingsBlocksInRound = ratingsBlocks.filter {
           case ((usersBlockId, itemsBlockId), _) => (usersBlockId + round) % numDiagnalRounds == itemsBlockId
         }
-        //println(s"[sam] debug: ratingsBlocksInRound: count: ${ratingsBlocksInRound.count()}")
 
-        // fixme: look up factors blocks by ratingsBlocksInRound?
         val uirBlocksInRound = usersBlocks.join(
           itemsBlocks.map {
             case (itemsBlockId, itemsBlock) =>
@@ -144,7 +67,6 @@ object SparkSgdNaive extends Logging {
             // changing schema for the join with rating blocks to filter
             ((usersBlockId, itemsBlockId), (usersBlock, itemsBlock))
         }.join(ratingsBlocksInRound)
-        //println(s"[sam] debug: uirBlocksInRound: count: ${uirBlocksInRound.count()}")
 
         val uirBlocksInRoundUpdated = uirBlocksInRound.mapValues {
           case ((usersBlock, itemsBlock), ratingsBlock) =>
@@ -152,7 +74,7 @@ object SparkSgdNaive extends Logging {
             // multiple write-read dependencies for each uesr and item factor
             ratingsBlock.ratings.zipWithIndex.foreach {
               case (rate, i) =>
-                // todo: improve O(n) to O(1) lookup
+                // todo: improve O(log n) to O(1) reverse lookup factors
                 val userId = ratingsBlock.users(i)
                 val itemId = ratingsBlock.items(i)
                 val userFactorIndex = usersBlock.users.indexOf(userId)
@@ -168,10 +90,8 @@ object SparkSgdNaive extends Logging {
             }
             (usersBlock, itemsBlock)
         }.persist(StorageLevel.MEMORY_AND_DISK)
-        //println(s"[sam] debug: uirBlocksInRoundUpdated: count: ${uirBlocksInRoundUpdated.count()}")
 
         // update usersBlocks and itemsBlocks with counterparts in uriBlocks
-        // fixme: use look up filter and then merge to update usersBlocks and itemsBlocks?
         val previousUsersBlocks = usersBlocks
         usersBlocks = uirBlocksInRoundUpdated.map {
           case ((usersBlockId, _), (usersBlock, _)) => (usersBlockId, usersBlock)
@@ -195,22 +115,22 @@ object SparkSgdNaive extends Logging {
         // materialize
         usersBlocks.count()
         itemsBlocks.count()
-        //println(s"[sam] at i$iter: usersBlocks size: ${usersBlocks.count()}, itemsBlocks size: ${itemsBlocks.count()}")
       }
       gamma *= STEP_DEC
       val iterEnd = System.currentTimeMillis()
       println(s"[sam] Time in iteration $iter of sgd ${iterEnd - iterStart} (ms)")
     }
 
-    println(s"[sam] usersBlocks size: ${usersBlocks.count()}, itemsBlocks size: ${itemsBlocks.count()}")
-    //logInfo(s"[sam] usersBlocks parts: ${usersBlocks.partitions.length}, itemsBlocks parts: ${itemsBlocks.partitions.length}")
+    logInfo(s"[sam] usersBlocks size: ${usersBlocks.count()}, itemsBlocks size: ${itemsBlocks.count()}")
+    logInfo(s"[sam] usersBlocks parts: ${usersBlocks.partitions.length}, itemsBlocks parts: ${itemsBlocks.partitions.length}")
     println(s"[sam] finished training for total iterations: $maxNumIters")
     val trainErr = computeTrainingError(usersBlocks, itemsBlocks, ratingsBlocks)
     println(s"[sam] training rmse $trainErr")
   }
 
-  def makeFactorsBlocks(userPart: HashPartitioner, itemPart: HashPartitioner, ratingsBlocks: RDD[((UsersBlockId, ItemsBlockId), RatingsBlock)], num_latent: Int): (RDD[(UsersBlockId, UsersBlock)], RDD[(ItemsBlockId, ItemsBlock)]) = {
-    // fixme: fist should aggregate by usersBlockId to create a usersBlock
+  private def makeFactorsBlocks(userPart: HashPartitioner, itemPart: HashPartitioner, ratingsBlocks: RDD[((UsersBlockId, ItemsBlockId), RatingsBlock)], num_latent: Int): (RDD[(UsersBlockId, UsersBlock)], RDD[(ItemsBlockId, ItemsBlock)]) = {
+    // aggregate by user-block ids to create user blocks, each with unique user
+    // ids that are shared across different item blocks
     val usersBlocks = ratingsBlocks.map {
       case ((usersBlockId, _), RatingsBlock(users, _, _)) => (usersBlockId, users)
     }.aggregateByKey(new UsersBlockBuilder(num_latent), new HashPartitioner(userPart.numPartitions))(
@@ -219,15 +139,9 @@ object SparkSgdNaive extends Logging {
       .mapValues(_.build())
       .setName("usersBlocks")
       .persist(StorageLevel.MEMORY_AND_DISK)
-      /*val usersBlocks = ratingsBlocks.map {
-      case ((usersBlockId, _), RatingsBlock(users_, _, _)) =>
-        val users: Array[UsersBlockId] = users_.toSet.toSeq.sorted.toArray
-        (usersBlockId, UsersBlock(users, Array.fill(users.length, num_latent)(randGen.nextDouble())))
-    }.partitionBy(new HashPartitioner(userPart.numPartitions))
-      .setName("usersBlock")
-      .persist(StorageLevel.MEMORY_AND_DISK)*/
 
-    // fixme: fist should aggregate by itemsBlockId to create an itemsBlock
+    // aggregate by item-block ids to create item blocks, each with unique
+    // item ids that are shared acrosss different user blocks
     val itemsBlocks = ratingsBlocks.map {
       case ((_, itemsBlockId), RatingsBlock(_, items, _)) => (itemsBlockId, items)
     }.aggregateByKey(new ItemsBlockBuilder(num_latent), new HashPartitioner(itemPart.numPartitions))(
@@ -236,18 +150,11 @@ object SparkSgdNaive extends Logging {
       .mapValues(_.build())
       .setName("itemsBlocks")
       .persist(StorageLevel.MEMORY_AND_DISK)
-    /*val itemsBlocks = ratingsBlocks.map {
-      case ((_, itemsBlockId), RatingsBlock(_, items_, _)) =>
-        val items = items_.toSet.toSeq.sorted.toArray
-        (itemsBlockId, ItemsBlock(items, Array.fill(items.length, num_latent)(randGen.nextDouble())))
-    }.partitionBy(new HashPartitioner(itemPart.numPartitions))
-      .setName("itemsBlock")
-      .persist(StorageLevel.MEMORY_AND_DISK)*/
 
     (usersBlocks, itemsBlocks)
   }
 
-  def partitionRatings(ratings: RDD[Rating], userPart: HashPartitioner, itemPart: HashPartitioner): RDD[((UsersBlockId, ItemsBlockId), RatingsBlock)] = {
+  private def partitionRatings(ratings: RDD[Rating], userPart: HashPartitioner, itemPart: HashPartitioner): RDD[((UsersBlockId, ItemsBlockId), RatingsBlock)] = {
     ratings.map { r =>
       ((userPart.getPartition(r.user), itemPart.getPartition(r.item)), r)
     }.aggregateByKey(new RatingsBlockBuilder)(
@@ -268,7 +175,7 @@ object SparkSgdNaive extends Logging {
         val itemsBlock = itemsBlockMap(itemsBlockId)
         ratingsBlock.ratings.zipWithIndex.map {
           case (rate, i) =>
-            // TODO: improve O(n) to O(1) lookup
+            // todo: improve O(n) to O(1) lookup
             val userId = ratingsBlock.users(i)
             val itemId = ratingsBlock.items(i)
             val userFactorIndex = usersBlock.users.indexOf(userId)
@@ -288,4 +195,77 @@ object SparkSgdNaive extends Logging {
     Math.sqrt(sumSquaredDev / numRatings)
   }
 
+  type UserId = Int
+  type ItemId = Int
+  type UsersBlockId = Int
+  type ItemsBlockId = Int
+
+  case class Rating(user: UserId, item: ItemId, rating: Double) extends Serializable
+  case class RatingsBlock(users: Array[UserId], items: Array[ItemId], ratings: Array[Double]) extends Serializable
+  case class UsersBlock(users: Array[UserId], factors: Array[Array[Double]]) extends Serializable
+  case class ItemsBlock(items: Array[ItemId], factors: Array[Array[Double]]) extends Serializable
+
+  class RatingsBlockBuilder extends Serializable {
+    val users  = mutable.ArrayBuilder.make[UserId]
+    val items = mutable.ArrayBuilder.make[ItemId]
+    val ratings = mutable.ArrayBuilder.make[Double]
+
+    def add(r: Rating): RatingsBlockBuilder = {
+      users += r.user
+      items += r.item
+      ratings += r.rating
+      this
+    }
+
+    def merge(b: RatingsBlock): RatingsBlockBuilder = {
+      users ++= b.users
+      items ++= b.items
+      ratings ++= b.ratings
+      this
+    }
+
+    def build(): RatingsBlock = {
+      RatingsBlock(users.result(), items.result(), ratings.result())
+    }
+  }
+
+  class UsersBlockBuilder(factorLength: Int) extends Serializable {
+    val users = mutable.ArrayBuilder.make[UserId]
+
+    def add(us: Array[UserId]): UsersBlockBuilder = {
+      users ++= us
+      this
+    }
+
+    def merge(b: UsersBlock): UsersBlockBuilder = {
+      users ++= b.users
+      this
+    }
+
+    def build(): UsersBlock = {
+      val uniqueUsers = users.result().toSet.toSeq.sorted.toArray
+      val factors = Array.fill(uniqueUsers.length, factorLength)(randGen.nextDouble())
+      UsersBlock(uniqueUsers, factors)
+    }
+  }
+
+  class ItemsBlockBuilder(factorLength: Int) extends Serializable {
+    val items = mutable.ArrayBuilder.make[ItemId]
+
+    def add(us: Array[UserId]): ItemsBlockBuilder = {
+      items ++= us
+      this
+    }
+
+    def merge(b: ItemsBlock): ItemsBlockBuilder = {
+      items ++= b.items
+      this
+    }
+
+    def build(): ItemsBlock = {
+      val uniqueItems = items.result().toSet.toSeq.sorted.toArray
+      val factors = Array.fill(uniqueItems.length, factorLength)(randGen.nextDouble())
+      ItemsBlock(uniqueItems, factors)
+    }
+  }
 }
