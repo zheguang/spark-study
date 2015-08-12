89e6b2c4ff8975ec2c52dec57854fb97356c4670
Sam Zhao <sam@cs.brown.edu>
2015-08-12 15:23:48 -0700

    result: cfsgd result.bff4d56

--- bin/update-result.sh
+++ bin/update-result.sh
@@ -7,14 +7,18 @@ project_root=$(dirname $0)/..
 modes=("cc" "java" "scala" "spark")
 
 if [ "${#@}" -eq 0 ]; then
-  run_modes=$modes
+  run_modes=${modes[@]}
 else
   run_modes=$@
 fi
 
+rev_id=`git rev-parse --short HEAD`
+
 for m in ${run_modes[@]}; do
-  echo "[info] cp $project_root/study/bench/cfsgd/$m/result/* $project_root/result/cfsgd/$m/"
-  cp $project_root/study/bench/cfsgd/$m/result/* $project_root/result/cfsgd/$m/
+  dest=$project_root/result/cfsgd/result.$rev_id/$m
+  echo "[info] cp $project_root/study/bench/cfsgd/$m/result/* $dest"
+  mkdir -p $dest
+  cp $project_root/study/bench/cfsgd/$m/result/* $dest
 done
 
 echo "[info] done"
